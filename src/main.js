9d9a934ddd910a931c4ef9330847311ad1362a6b
Mateo Garcia <mateo.garcia@evolvevacationrental.com>
2024-04-02 20:27:28 -0500

    feat: redefine project structure

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 import express from "express";
-import { UserRouter } from "./user/user.router.js";
-import { GroupRouter } from "./group/group.router.js";
-import { FriendRouter } from "./friend/friend.router.js";
+import { UserRouter } from "./router/user.router.js";
+import { GroupRouter } from "./router/group.router.js";
+import { FriendRouter } from "./router/friend.router.js";
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use("/users", new UserRouter().registerRoutes());
-app.use("/groups", new GroupRouter().registerRoutes());
-app.use("/friends", new FriendRouter().registerRoutes());
+console.log(0, "root");
+
+app.use("/users", UserRouter().registerRoutes());
+app.use("/groups", GroupRouter().registerRoutes());
+app.use("/friends", FriendRouter().registerRoutes());
 
 app.listen(PORT, () => {
   console.log(`Express server running on port http://localhost:${PORT} 🚀`);
