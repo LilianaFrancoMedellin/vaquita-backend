9e7bb9b972378ed607b259ff239e54ef7628f89d
Miguel Sierra <miguel-sg01@hotmail.com>
2024-04-16 21:00:30 -0500

    feat: mock session express

--- src/app.js
+++ src/app.js
@@ -1,12 +1,27 @@
-import express from "express";
-import { GroupRouter } from "./router/group.router.js";
+import express from 'express';
+import { GroupRouter } from './router/group.router.js';
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+//TEMPORAL TO MOCK SESSION
+const mockSession = {
+  user: {
+    id: 10000,
+    name: 'YOUR USER',
+    email: 'user@email.com',
+  },
+};
+
+const sessionMiddleware = (req, res, next) => {
+  req.session = mockSession;
+  next();
+};
+
+app.use(sessionMiddleware);
 app.use(express.json());
 
-app.use("/groups", GroupRouter().registerRoutes());
+app.use('/groups', GroupRouter().registerRoutes());
 
 app.listen(PORT, () => {
   console.log(`Express server running on port http://localhost:${PORT} 🚀`);
