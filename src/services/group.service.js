7d64f83eaa102dac275d8bd02bdac13490633b35
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-05-04 10:40:31 -0500

    Implementing Joi validations and exceptions

--- src/services/group.service.js
+++ src/services/group.service.js
@@ -1,4 +1,5 @@
 import { GroupModel } from "../models/group.model.js";
+import ConflictException from "../exceptions/conflict.exception.js";
 
 const groupModel = GroupModel();
 
@@ -23,34 +24,13 @@ const GroupService = () => {
     console.log(3.1, "[Group] Service Create");
     const { name } = newGroup;
 
-    if (name.length > 30) {
-      return {
-        newGroup: null,
-        success: false,
-        message: "The field name can not be longer thatn 30 characters",
-        code: 400,
-      };
-    }
-
     const groupFound = await groupModel.findByName(name);
 
     if (groupFound) {
-      return {
-        newGroup: null,
-        success: false,
-        message: "The group already exists",
-        code: 409,
-      };
+      throw new ConflictException("The group already exists");
     }
 
-    const createdGroup = await groupModel.create(newGroup);
-
-    return {
-      newGroup: createdGroup,
-      success: true,
-      message: "Group created successfully",
-      code: 201,
-    };
+    return groupModel.create(newGroup);
   };
 
   const editById = (id, group) => {
